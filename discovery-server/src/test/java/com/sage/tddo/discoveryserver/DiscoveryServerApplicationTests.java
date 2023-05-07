286f382bb99fd12669524c9c9a3bff828a260a38
Sage-Park <sagepark94@gmail.com>
2023-05-07 14:13:11 +0900

    test profiles

--- discovery-server/src/test/java/com/sage/tddo/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/sage/tddo/discoveryserver/DiscoveryServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.sage.tddo.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DiscoveryServerApplicationTests {
 
     @Test
