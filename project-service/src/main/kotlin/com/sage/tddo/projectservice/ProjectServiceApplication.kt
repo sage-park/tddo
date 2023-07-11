170ad020704a1c56f8be2068ae4b7661f9c73a4a
Sage-Park <sagepark94@gmail.com>
2023-07-12 00:44:56 +0900

    create project-service

---
+++ project-service/src/main/kotlin/com/sage/tddo/projectservice/ProjectServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sage.tddo.projectservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectServiceApplication>(*args)
+}
---
+++ project-service/src/test/kotlin/com/sage/tddo/projectservice/ProjectServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sage.tddo.projectservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
